ca6ad672e14dea8104f6a063425abac1ec8e71ff
lperezp <luiseperezp.10@gmail.com>
2020-08-23 14:38:50 -0500

    refactor(profile): add widget

--- lib/pages/profile-page.dart
+++ lib/pages/profile-page.dart
@@ -22,7 +22,9 @@ class _ProfilePageState extends State<ProfilePage> {
         elevation: 0,
         leading: IconButton(
           icon: Image.asset('assets/img/vector-left.png'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
         actions: <Widget>[
           IconButton(
@@ -122,99 +124,7 @@ class _ProfilePageState extends State<ProfilePage> {
             )),
         Container(
           padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFEEF7FE),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Image.asset('assets/img/more-options-v-1.png'),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Image.asset('assets/img/folder-1.png'),
-                        SizedBox(height: 20),
-                        Text(
-                          "Mobile Apps",
-                          style: TextStyle(
-                            color: Color(0xFF567DF4),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 15,
-                          ),
-                        ),
-                        Text(
-                          "December 20.2020",
-                          style: TextStyle(
-                            color: Color(0xFF567DF4),
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFFFFBEC),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Image.asset('assets/img/more-options-v-2.png'),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Image.asset('assets/img/folder-2.png'),
-                        SizedBox(height: 20),
-                        Text(
-                          "SVG Icons",
-                          style: TextStyle(
-                            color: Color(0xFFFFB110),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 15,
-                          ),
-                        ),
-                        Text(
-                          "December 14.2020",
-                          style: TextStyle(
-                            color: Color(0xFFFFB110),
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: MyFoldersRight(),
         ),
         Container(
             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
@@ -241,59 +151,7 @@ class _ProfilePageState extends State<ProfilePage> {
             )),
         Container(
           padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(12),
-                          decoration: BoxDecoration(
-                            color: Color(0xFFEEF7FE),
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: Image.asset('assets/img/word.png'),
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Projects.docx',
-                              style: TextStyle(
-                                color: Color(0xFF22215B),
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            Text(
-                              'November 22.2020',
-                              style: TextStyle(
-                                color: Color.fromRGBO(34, 33, 91, 0.6),
-                                fontSize: 11,
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    Text(
-                      '300kb',
-                      style: TextStyle(
-                        fontSize: 11,
-                        color: Color.fromRGBO(34, 33, 91, 0.6),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: RecentUpload(),
         ),
         Container(
             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
@@ -328,101 +186,257 @@ class _ProfilePageState extends State<ProfilePage> {
             )),
         Container(
           padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFFEEEEE),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset('assets/img/folder-3.png'),
-                        SizedBox(height: 20),
-                        Text(
-                          "Prototypes",
-                          style: TextStyle(
-                            color: Color(0xFFAC4040),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 15,
-                          ),
-                        ),
-                        Text(
-                          "November 22.2020",
-                          style: TextStyle(
-                            color: Color(0xFFAC4040),
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Image.asset('assets/img/more-options-v-3.png'),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFF0FFFF),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset('assets/img/folder-4.png'),
-                        SizedBox(height: 20),
-                        Text(
-                          "SVG Icons",
-                          style: TextStyle(
-                            color: Color(0xFF23B0B0),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 15,
-                          ),
-                        ),
-                        Text(
-                          "December 14.2020",
-                          style: TextStyle(
-                            color: Color(0xFF23B0B0),
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Image.asset('assets/img/more-options-v-1.png'),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: MyFoldersLeft(),
         ),
       ]),
     );
   }
+}
+
+Widget MyFoldersRight(){
+  return Column(
+    children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFEEF7FE),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Image.asset('assets/img/more-options-v-1.png'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Image.asset('assets/img/folder-1.png'),
+                SizedBox(height: 20),
+                Text(
+                  "Mobile Apps",
+                  style: TextStyle(
+                    color: Color(0xFF567DF4),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  "December 20.2020",
+                  style: TextStyle(
+                    color: Color(0xFF567DF4),
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFFFFBEC),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Image.asset('assets/img/more-options-v-2.png'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Image.asset('assets/img/folder-2.png'),
+                SizedBox(height: 20),
+                Text(
+                  "SVG Icons",
+                  style: TextStyle(
+                    color: Color(0xFFFFB110),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  "December 14.2020",
+                  style: TextStyle(
+                    color: Color(0xFFFFB110),
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget RecentUpload(){
+  return Column(
+    children: <Widget>[
+      Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEEF7FE),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Image.asset('assets/img/word.png'),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Projects.docx',
+                      style: TextStyle(
+                        color: Color(0xFF22215B),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'November 22.2020',
+                      style: TextStyle(
+                        color: Color.fromRGBO(34, 33, 91, 0.6),
+                        fontSize: 11,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              '300kb',
+              style: TextStyle(
+                fontSize: 11,
+                color: Color.fromRGBO(34, 33, 91, 0.6),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget MyFoldersLeft(){
+  return Column(
+    children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFFEEEEE),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/img/folder-3.png'),
+                SizedBox(height: 20),
+                Text(
+                  "Prototypes",
+                  style: TextStyle(
+                    color: Color(0xFFAC4040),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  "November 22.2020",
+                  style: TextStyle(
+                    color: Color(0xFFAC4040),
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Image.asset('assets/img/more-options-v-3.png'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFF0FFFF),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/img/folder-4.png'),
+                SizedBox(height: 20),
+                Text(
+                  "SVG Icons",
+                  style: TextStyle(
+                    color: Color(0xFF23B0B0),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  "December 14.2020",
+                  style: TextStyle(
+                    color: Color(0xFF23B0B0),
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Image.asset('assets/img/more-options-v-1.png'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
 }
